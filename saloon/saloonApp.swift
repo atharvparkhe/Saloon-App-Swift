f66f844197bb48f4187a42a245a68a49279ced57
Atharva Parkhe <patharv777@gmail.com>
2022-02-12 11:58:17 +0530

    Initial Commit

---
+++ saloon/saloonApp.swift
@@ -0,0 +1,17 @@
+//
+//  saloonApp.swift
+//  saloon
+//
+//  Created by Atharva Parkhe on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct saloonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
