f077f0f14744a7dec569cbeadfdcb85be2cd9073
Atharva Parkhe <patharv777@gmail.com>
2022-02-16 14:57:49 +0530

    initial commit

---
+++ saloon/Utils/Constants.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct Constants{
+    
+    static let BASE_URL = URL(string: "http://127.0.0.1:8000/")!
+    
+    static let BASE_API_URL = URL(string: "http://127.0.0.1:8000/api/")!
+    
+    
+}
+
+
+
--- saloon/saloonApp.swift
+++ saloon/saloonApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct saloonApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SaloonListView()
         }
     }
 }
